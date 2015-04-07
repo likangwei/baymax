6f80a0e7a1fc27a996b9354c25399241170c601c
aa3632840 <262708239@qq.com>
2015-04-07 23:36:42 +0800

    添加USER 到 admin

--- Baymax/urls.py
+++ Baymax/urls.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 __author__ = 'likang'
 from django.conf.urls import patterns, url
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^login', 'baymax.views.login'),
-    (r'^reg', 'baymax.views.reg'),
+    (r'^login', 'Baymax.views.login'),
+    (r'^reg', 'Baymax.views.reg'),
 )
