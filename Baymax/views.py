80abcec400438df58b9072110dc0bf8eae4fd676
aa3632840 <262708239@qq.com>
2015-04-07 23:18:13 +0800

    xx

--- Baymax/views.py
+++ Baymax/views.py
@@ -5,7 +5,7 @@ from django import forms
 from models import User
 
 class RegForm(forms.ModelForm):
-    class Meta: 
+    class Meta:
         model = User
 
 def reg(request):
@@ -13,7 +13,7 @@ def reg(request):
         form = RegForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
             # Process the data in form.cleaned_data
-            # ...
+            # ...hehe
             print form.cleaned_data['name']
             return HttpResponseRedirect('/thanks/') # Redirect after POST
     else:
