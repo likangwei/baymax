f8edfefcf6a969b35985a5f2f97184c1a8fc5769
aa3632840 <262708239@qq.com>
2015-04-07 21:21:18 +0800

    first commit

---
+++ Baymax/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Human(models.Model):
+    """主人"""
+    name = models.CharField('名字',max_length=100)
+    age = models.IntegerField('年龄', max_length=11)
+    birthday = models.DateField('生日')
+
