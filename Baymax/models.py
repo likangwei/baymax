c7794b1fec8715f78194bdb02c403a52260571ef
aa3632840 <262708239@qq.com>
2015-04-07 22:28:26 +0800

    添加注册模块

--- Baymax/models.py
+++ Baymax/models.py
@@ -1,8 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
 
-class Human(models.Model):
+class User(models.Model):
     """主人"""
     name = models.CharField('名字',max_length=100)
     age = models.IntegerField('年龄', max_length=11)
--- Baymax/views.py
+++ Baymax/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django import forms
 
-# Create your views here.
+class RegForm(forms.Form):
+    name = forms.CharField(max_length=100)
+
+
+def reg(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = RegForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            print form.cleaned_data['name']
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = RegForm() # An unbound form
+    return render(request, 'baymax/reg.html', {
+        'form': form,
+    })
+
+def login(request):
+    return HttpResponse("hehe")
