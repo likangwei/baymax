a2d19d1481ab9a8d43bdd9193efa1ac4e3d9f8ec
likangwei <262708239@qq.com>
2015-04-09 13:41:40 +0800

    Please enter the commit message for your changes. Lines starting commit

--- Baymax/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-__author__ = 'likang'
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    (r'^login', 'baymax.views.login'),
-    (r'^reg', 'baymax.views.reg'),
-)
