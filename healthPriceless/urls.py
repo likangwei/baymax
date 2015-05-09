9d0f8f6e0aae2962ee15c925af43ff619d25b049
aa3632840 <262708239@qq.com>
2015-05-09 15:25:15 +0800

    fix url bug

--- healthPriceless/urls.py
+++ healthPriceless/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^', include('wordRecall.urls', namespace='word')),
+    url(r'^$', include('wordRecall.urls', namespace='word')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^baymax', include('baymax.urls', namespace='baymax')),
 
