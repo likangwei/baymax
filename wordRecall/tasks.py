88257ab9c6416c6a673553581559f0f159d99b41
likangwei <lovekangge@163.com>
2015-05-21 16:08:43 +0800

    改变页面title

--- wordRecall/tasks.py
+++ wordRecall/tasks.py
@@ -5,8 +5,8 @@ print healthPriceless.settings.DEBUG
 from celery import shared_task
 from wordRecall.models import Word
 import time
-from wordRecall import parser
 from wordRecall.parser import get_html_word_repeated_info
+
 
 @shared_task
 def add(x, y):
@@ -22,6 +22,7 @@ def mul(x, y):
 @shared_task
 def xsum(numbers):
     return sum(numbers)
+
 
 @shared_task
 def add_all_page_word_to_repeated(html_url):
