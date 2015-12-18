9db5273f47064edd78a3a3b6ebf67c8ddf8d0907
likangwei <lovekangge@163.com>
2015-12-18 23:54:34 +0800

    fix bug

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def test():
     while no_meaning_words.count() > 0:
         get_google_meanings(no_meaning_words)
         no_meaning_words = Word.objects.filter(google_meaning='')[:500]
-        time.sleep(1)
+        time.sleep(5)
 
 
 if __name__ == '__main__':
