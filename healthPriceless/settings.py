3d730dd5a0c028f6a4a29ca1596a00caeb131fd3
李康炜 <262708239@qq.com>
2016-03-04 16:44:34 +0800

    change zh-CN

--- healthPriceless/settings.py
+++ healthPriceless/settings.py
@@ -110,7 +110,7 @@ DATABASES = HOST_DATA['db']
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-CN'
 
 TIME_ZONE = 'Asia/Shanghai'
 
@@ -121,7 +121,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
